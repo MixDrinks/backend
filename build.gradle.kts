eb464f10cadf6299b075bd6dfbc9b46e852b388f
Vova Stelmashchuk <smallstells@gmail.com>
2022-06-05 17:43:16 +0300

    Fixed shadow jar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     application
     kotlin("jvm") version "1.6.21"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "org.mixdrinks"
