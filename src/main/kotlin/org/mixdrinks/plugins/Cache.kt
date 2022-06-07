69efdbd244501e5badfc2b5d486915e78f6b5861
Vova Stelmashchuk <smallstells@gmail.com>
2022-06-07 19:09:14 +0300

    Fixed caches

--- src/main/kotlin/org/mixdrinks/plugins/Cache.kt
+++ src/main/kotlin/org/mixdrinks/plugins/Cache.kt
@@ -4,13 +4,14 @@ import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.request.*
 import java.util.concurrent.TimeUnit
 
 fun Application.configureCache() {
     install(CachingHeaders) {
         options { call, outgoingContent ->
-            when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Application.Json -> CachingOptions(
+            when {
+                isGet(call) && isJson(outgoingContent) -> CachingOptions(
                     CacheControl.MaxAge(
                         maxAgeSeconds = TimeUnit.MINUTES.toSeconds(1).toInt(),
                         visibility = CacheControl.Visibility.Public
@@ -20,5 +21,8 @@ fun Application.configureCache() {
             }
         }
     }
-
 }
+
+private fun isGet(call: ApplicationCall) = call.request.httpMethod == HttpMethod.Get
+
+private fun isJson(content: OutgoingContent) = content.contentType?.withoutParameters() == ContentType.Application.Json
