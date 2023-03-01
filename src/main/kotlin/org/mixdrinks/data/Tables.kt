06151b2e864a533c838b6e17089be32d95aaa1da
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-01 14:00:02 +0200

    Migrate to int id table

--- src/main/kotlin/org/mixdrinks/data/Tables.kt
+++ src/main/kotlin/org/mixdrinks/data/Tables.kt
@@ -1,26 +1,21 @@
 package org.mixdrinks.data
 
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Table
 
-object CocktailsTable : Table(name = "cocktails") {
-    val id = integer("id")
+object CocktailsTable : IntIdTable(name = "cocktails", columnName = "id") {
     val name = text("name")
     val steps = textArray("recipe")
     val visitCount = integer("visit_count")
     val ratingCount = integer("rating_count")
     val ratingValue = integer("rating_value").nullable()
-
-    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
 
-object ItemsTable : Table(name = "goods") {
-    val id = integer("id")
+object ItemsTable : IntIdTable(name = "goods", columnName = "id") {
     val name = text("name")
     val about = text("about")
     val relation = integer("relation")
     val visitCount = integer("visit_count")
-
-    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
 
 object CocktailsToItemsTable : Table(name = "cocktails_to_items") {
@@ -31,33 +26,26 @@ object CocktailsToItemsTable : Table(name = "cocktails_to_items") {
     val relation = integer("relation")
 }
 
-object TagsTable : Table(name = "tags") {
-    val id = integer("id")
+object TagsTable : IntIdTable(name = "tags", columnName = "id") {
     val name = text("name")
 }
 
-object TastesTable : Table(name = "tastes") {
-    val id = integer("id")
+object TastesTable : IntIdTable(name = "tastes", columnName = "id") {
     val name = text("name")
-
-    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
 
 object CocktailsToTastesTable : Table(name = "cocktails_to_tastes") {
-    val tasteId = integer("taste_id") references TastesTable.id
-    val cocktailId = integer("cocktail_id") references CocktailsTable.id
+    val tasteId = integer("taste_id").references(TastesTable.id)
+    val cocktailId = integer("cocktail_id").references(CocktailsTable.id)
 }
 
-object AlcoholVolumesTable : Table(name = "alcohol_volumes") {
-    val id = integer("id")
+object AlcoholVolumesTable : IntIdTable(name = "alcohol_volumes", columnName = "id") {
     val name = text("name")
-
-    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
 
 object CocktailsToAlcoholVolumesTable : Table(name = "cocktails_to_alcohol_volume") {
-    val cocktailId = integer("cocktail_id") references CocktailsTable.id
-    val alcoholVolumeId = integer("alcohol_volume_id") references AlcoholVolumesTable.id
+    val cocktailId = integer("cocktail_id").references(CocktailsTable.id)
+    val alcoholVolumeId = integer("alcohol_volume_id").references(AlcoholVolumesTable.id)
 }
 
 object CocktailToTagTable : Table(name = "cocktails_to_tags") {
