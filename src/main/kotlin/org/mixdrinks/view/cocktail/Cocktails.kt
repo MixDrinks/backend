c445ceecf78e01c82291a4076e89c495a824b449
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-12 20:58:25 +0200

    suppress getTaste not used

--- src/main/kotlin/org/mixdrinks/view/cocktail/Cocktails.kt
+++ src/main/kotlin/org/mixdrinks/view/cocktail/Cocktails.kt
@@ -65,6 +65,7 @@ private fun getFullCocktail(id: Int): FullCocktailVM {
     }
 }
 
+@Suppress("UnusedPrivateMember")
 private fun getTastes(cocktail: FullCocktail): List<TagVM> {
     return cocktail.taste.map {
         return@map buildTagVM(it.id, it.name, FilterModels.Filters.TASTE)
