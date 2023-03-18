d5771b10dc389c1b670bc42875d1665d37787519
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-18 14:17:03 +0200

    up version to 1.4.1

--- src/main/kotlin/org/mixdrinks/view/Service.kt
+++ src/main/kotlin/org/mixdrinks/view/Service.kt
@@ -9,7 +9,7 @@ import io.ktor.server.routing.routing
 fun Application.service() {
     routing {
         get("/") {
-            call.respond("Version 1.4.0")
+            call.respond("Version 1.4.1")
         }
     }
 }
