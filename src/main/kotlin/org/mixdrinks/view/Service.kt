d826999fbcf7f7a6ae3a2785cfdf5030c8e65537
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-28 13:19:28 +0300

    Implement open api featuer

--- src/main/kotlin/org/mixdrinks/view/Service.kt
+++ src/main/kotlin/org/mixdrinks/view/Service.kt
@@ -2,14 +2,24 @@ package org.mixdrinks.view
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.plugins.openapi.openAPI
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 fun Application.service(appVersion: String) {
     routing {
         get("/version") {
-            call.respond("version" to appVersion)
+            call.respond(Version(appVersion))
         }
+        openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
     }
 }
+
+@Serializable
+data class Version(
+    @SerialName("version")
+    val version: String
+)
