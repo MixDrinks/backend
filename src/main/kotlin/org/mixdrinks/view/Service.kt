f9710a8724da011497ee3aa382f67026c6800e89
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-28 13:36:36 +0300

    Remove version pojo object

--- src/main/kotlin/org/mixdrinks/view/Service.kt
+++ src/main/kotlin/org/mixdrinks/view/Service.kt
@@ -6,20 +6,12 @@ import io.ktor.server.plugins.openapi.openAPI
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
 fun Application.service(appVersion: String) {
     routing {
         get("/version") {
-            call.respond(Version(appVersion))
+            call.respond("version" to appVersion)
         }
         openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
     }
 }
-
-@Serializable
-data class Version(
-    @SerialName("version")
-    val version: String
-)
