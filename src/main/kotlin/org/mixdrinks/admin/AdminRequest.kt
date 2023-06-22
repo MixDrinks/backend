4bffac8dd94e0bb6f74854a51d25df0b6655ece3
Vova Stelmashchuk <smallstells@gmail.com>
2023-06-22 10:27:15 +0300

    Implement admin response as json

--- src/main/kotlin/org/mixdrinks/admin/AdminRequest.kt
+++ src/main/kotlin/org/mixdrinks/admin/AdminRequest.kt
@@ -8,3 +8,8 @@ data class AdminRequest(
     @SerialName("login") val login: String,
     @SerialName("password") val password: String
 )
+
+@Serializable
+data class AdminResponse(
+    @SerialName("login") val login: String,
+)
